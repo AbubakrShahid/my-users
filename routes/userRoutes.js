b70e8e0a0b6f767cffb21cfc3060761ab762713b
AbubakrShahid <abubakarshahidd95@gmail.com>
2025-03-24 15:33:01 -0700

    Updated route and configured servers

---
+++ routes/userRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { getUserById } = require("../controllers/userController");
+const validateObjectIdAndAge = require("../middlewares/validateObjectIdAndAge");
+
+const router = express.Router();
+
+router.get("/:id", validateObjectIdAndAge, getUserById);
+
+module.exports = router;
