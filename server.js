bcaf0ed74918c4157442b7a858175f152c4bc529
AbubakrShahid <abubakarshahidd95@gmail.com>
2025-03-25 13:53:58 -0700

    Fixed port issue for docker

--- server.js
+++ server.js
@@ -16,11 +16,3 @@ app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 module.exports = app;
 
-if (require.main === module) {
-  const PORT = process.env.PORT || 5000;
-  const server = app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-  
-  process.on('SIGTERM', () => server.close());
-}
